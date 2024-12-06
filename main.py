5cc94a880682efebd03de391f2c7b7ad1931c654
Swetang Pandit <swetangpandit@gmail.com>
2024-12-06 15:12:50 +0530

    Final Code.

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
+
 import json
 
 def load_data():
     try:
         with open('youtube.txt', 'r') as file:
             test = json.load(file)
+            # print(type(test))
             return test
     except FileNotFoundError:
-        return []    
-
-       
+        return []
+    
 def save_data_helper(videos):
     with open('youtube.txt', 'w') as file:
-        json.dump(videos, file)   
+        json.dump(videos, file)
 
 def list_all_videos(videos):
-    pass
+    print("\n")
+    print("*" * 70)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']} ")
+    print("\n")
+    print("*" * 70)
 
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update"))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name")
+        time = input("Enter the new video time")
+        videos[index-1] = {'name':name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
+
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted"))
+    
+    if 1<= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected")
 
 
 def main():
@@ -51,3 +76,6 @@ def main():
                 break
             case _:
                 print("Invalid Choice")
+
+if __name__ ==  "__main__":
+    main() 
