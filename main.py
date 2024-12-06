e18bcaf3f5acf72541581f98e39e20e7f07d19a9
Swetang Pandit <swetangpandit@gmail.com>
2024-12-06 13:48:58 +0530

    Helper fuction.

---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+
+def load_data():
+    try:
+        with open('youtube.txt', 'r') as file:
+            test = json.load(file)
+            return test
+    except FileNotFoundError:
+        return []    
+
+       
+def save_data_helper(videos):
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos, file)   
+
+def list_all_videos(videos):
+    pass
+
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
+        # print(videos)
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")
